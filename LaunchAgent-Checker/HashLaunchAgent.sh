aca2ef36f87a9b9416de5e4393ddb94c5fd62b3a
Simon <SCTCoding@users.noreply.github.com>
2017-01-27 22:58:32 -0500

    Update HashLaunchAgent.sh

--- LaunchAgent-Checker/HashLaunchAgent.sh
+++ LaunchAgent-Checker/HashLaunchAgent.sh
@@ -1,1 +1,27 @@
+#! /bin/bash
 
+uname=$(whoami)
+
+read -p "Are you creating a reference file? (y/n) " option
+echo ""
+if [[ $option == "n"  ]]
+then
+  echo "Verifying LaunchAgentss..."
+  echo ""
+  read -p "What version of OS X? (Example: 10.11.6) " version
+
+  shasum /System/Library/LaunchAgents/* -c ./Reference/$version.txt >> /Users/$uname/Desktop/Results.txt
+  echo ""
+  echo "Process Complete"
+  
+else
+  echo "Creating Reference File..."
+  echo ""
+  read -p "Which version of OS X are you checking? (Example: 10.11.6) " newversion
+
+  shasum /System/Library/LaunchAgents/* >> ./Reference/$newversion
+  echo ""
+  echo "Process Complete"
+fi
+
+exit
